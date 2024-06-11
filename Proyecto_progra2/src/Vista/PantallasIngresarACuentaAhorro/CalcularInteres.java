ecefd373f36be8c4cfdae83bb3ca926aa5644b5a
Jaime-Macbook <huaychojaime@gmail.com>
2024-06-10 23:18:00 -0400

    skebf

--- Proyecto_progra2/src/Vista/PantallasIngresarACuentaAhorro/CalcularInteres.java
+++ Proyecto_progra2/src/Vista/PantallasIngresarACuentaAhorro/CalcularInteres.java
@@ -1,6 +1,5 @@
 package Vista.PantallasIngresarACuentaAhorro;
 
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,32 +10,14 @@ public class CalcularInteres extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CalcularInteres frame = new CalcularInteres();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public CalcularInteres() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 673, 610);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
