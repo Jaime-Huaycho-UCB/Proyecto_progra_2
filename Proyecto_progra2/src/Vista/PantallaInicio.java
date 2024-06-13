4ba28ed27437f692ef02e2dfc6881143b8c10863
Jaime-Macbook <huaychojaime@gmail.com>
2024-06-13 15:34:23 -0400

    iubsef

--- Proyecto_progra2/src/Vista/PantallaInicio.java
+++ Proyecto_progra2/src/Vista/PantallaInicio.java
@@ -32,7 +32,7 @@ public class PantallaInicio extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 722, 394);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBackground(new Color(28, 49, 84));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -43,7 +43,8 @@ public class PantallaInicio extends JFrame {
 		
 		
 		JButton BotonIngresarCuenta = new JButton("CLIENTE");
-		BotonIngresarCuenta.setForeground(new Color(0, 0, 0));
+		BotonIngresarCuenta.setOpaque(true);
+		BotonIngresarCuenta.setForeground(new Color(28, 49, 84));
 		BotonIngresarCuenta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				SeleccionCuenta ventanaSeleccionCuenta = new SeleccionCuenta();
@@ -51,25 +52,27 @@ public class PantallaInicio extends JFrame {
 				dispose();
 			}
 		});
-		BotonIngresarCuenta.setFont(new Font("Courier New", Font.ITALIC, 20));
+		BotonIngresarCuenta.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 20));
 		BotonIngresarCuenta.setBounds(391, 128, 259, 71);
 		contentPane.add(BotonIngresarCuenta);
 		
 		JButton BotonAdministrar = new JButton("ADMINISTRACION");
-		BotonAdministrar.setBackground(new Color(255, 146, 0));
-		BotonAdministrar.setForeground(new Color(9, 18, 17));
+		BotonAdministrar.setOpaque(true);
+		BotonAdministrar.setBackground(new Color(254, 255, 255));
+		BotonAdministrar.setForeground(new Color(28, 49, 84));
 		BotonAdministrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				lib.CambiarPantalla(new LogIn());
 				dispose();
 			}
 		});
-		BotonAdministrar.setFont(new Font("Courier New", Font.ITALIC, 20));
+		BotonAdministrar.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 20));
 		BotonAdministrar.setBounds(63, 128, 259, 71);
 		contentPane.add(BotonAdministrar);
 		
 		JButton btnNewButton = new JButton("SALIR");
-		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setOpaque(true);
+		btnNewButton.setForeground(new Color(28, 49, 84));
 		btnNewButton.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 13));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -82,22 +85,14 @@ public class PantallaInicio extends JFrame {
 		
 		
 		txtGoliathBankGfxfgxrtxtrrt = new JTextField();
+		txtGoliathBankGfxfgxrtxtrrt.setEditable(false);
+		txtGoliathBankGfxfgxrtxtrrt.setHorizontalAlignment(SwingConstants.CENTER);
 		txtGoliathBankGfxfgxrtxtrrt.setForeground(new Color(254, 255, 255));
-		txtGoliathBankGfxfgxrtxtrrt.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 28));
-		txtGoliathBankGfxfgxrtxtrrt.setBackground(new Color(4, 50, 255));
-		txtGoliathBankGfxfgxrtxtrrt.setText("                GOLIATH  NATIONAL BANK");
-		txtGoliathBankGfxfgxrtxtrrt.setBounds(0, 0, 722, 91);
+		txtGoliathBankGfxfgxrtxtrrt.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 30));
+		txtGoliathBankGfxfgxrtxtrrt.setBackground(new Color(120, 32, 24));
+		txtGoliathBankGfxfgxrtxtrrt.setText("GOLIATH NATIONAL BANK");
+		txtGoliathBankGfxfgxrtxtrrt.setBounds(-25, 6, 777, 91);
 		contentPane.add(txtGoliathBankGfxfgxrtxtrrt);
 		txtGoliathBankGfxfgxrtxtrrt.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(111, 249, 61, 16);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon("/Users/jaimehuaycho/Downloads/1631338443895.jpeg"));
-		lblNewLabel_1.setBounds(0, 268, 200, 98);
-		contentPane.add(lblNewLabel_1);
 	}
 }
